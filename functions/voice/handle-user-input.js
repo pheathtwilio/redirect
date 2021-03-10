e4170c6d6955538abdfeff73a07293e72dc6aead
pheath <pheath@twilio.com>
2021-03-10 14:47:39 -0600

    added webhook url and studioflow url variables

--- functions/voice/handle-user-input.js
+++ functions/voice/handle-user-input.js
@@ -4,6 +4,9 @@ exports.handler = function (context, event, callback) {
 
     let userInput = event.Digits || event.SpeechResult;
     let twiml = new Twilio.twiml.VoiceResponse();
+
+    const webhookURL = 'https://webhooks.twilio.com/v1/Accounts/'
+    const studioFlow = '/Flows/FW8e48233ddb0ebd096d7c1c7fa4906e1f?FlowEvent=return'
   
     if (!userInput) {
       twiml.say('Sorry something went wrong. Please call again');
@@ -15,7 +18,7 @@ exports.handler = function (context, event, callback) {
 
     twiml.redirect({
         method: 'POST'
-    }, 'https://webhooks.twilio.com/v1/Accounts/' + context.ACCOUNT_SID + '/Flows/FW8e48233ddb0ebd096d7c1c7fa4906e1f?FlowEvent=return');
+    }, webhookURL + context.ACCOUNT_SID + studioFlow);
 
     callback(null, twiml);
 }
